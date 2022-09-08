71824e92b5b6fc22dcfd8ce24d6cf0bec031841a
linusberg <linus@fenix.me.uk>
2022-09-08 20:08:25 +0200

    Added return to NPM module for continued processing.

--- APC.Ingestion/Engine.cs
+++ APC.Ingestion/Engine.cs
@@ -5,14 +5,23 @@ using MassTransit;
 namespace APC.Ingestion; 
 
 public class Engine : IConsumer<ArtifactProcessedRequest> {
-
-  public Task Consume(ConsumeContext<ArtifactProcessedRequest> context) {
+  HashSet<string> processed = new HashSet<string>();
+  public async Task Consume(ConsumeContext<ArtifactProcessedRequest> context) {
     Artifact artifact = context.Message.Artifact;
+    Console.WriteLine($"Processed {artifact.Id}");
     foreach (KeyValuePair<string, ArtifactVersion> version in artifact.Versions) {
       foreach (string dep in version.Value.Dependencies) {
-        Console.WriteLine(dep);
+        if (processed.Contains(dep)) {
+          continue;
+        } 
+        processed.Add(dep);
+        await context.Send(new Uri($"queue:{artifact.Type}-module"), new ArtifactProcessRequest() {
+          Artifact = new Artifact() {
+            Id = dep,
+            Type = artifact.Type
+          }
+        }); 
       }
     }
-    return Task.CompletedTask;
   }
 }
--- APC.Ingestion/Worker.cs
+++ APC.Ingestion/Worker.cs
@@ -14,23 +14,23 @@ public class Worker : BackgroundService {
   }
 
   protected override async Task ExecuteAsync(CancellationToken stoppingToken) {
+    ISendEndpoint endpoint = await bus_.GetSendEndpoint(new Uri("queue:npm-module"));
+    ISendEndpoint nuget_endpoint = await bus_.GetSendEndpoint(new Uri("queue:nuget-module"));
+    ArtifactProcessRequest apr = new ArtifactProcessRequest();
+    apr.Artifact = new Artifact() {
+      Id = "react",
+      Type = "npm"
+    };
+    ArtifactProcessRequest apr_n = new ArtifactProcessRequest();
+    apr_n.Artifact = new Artifact() {
+      Id = "nuget",
+      Type = "nuget"
+    };
+    await endpoint.Send(apr);
+    await nuget_endpoint.Send(apr_n);
     while (!stoppingToken.IsCancellationRequested) {
       _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-      ISendEndpoint endpoint = await bus_.GetSendEndpoint(new Uri("queue:npm-module"));
-      ISendEndpoint nuget_endpoint = await bus_.GetSendEndpoint(new Uri("queue:nuget-module"));
-      ArtifactProcessRequest apr = new ArtifactProcessRequest();
-      apr.Artifact = new Artifact() {
-        Id = "react",
-        Type = "npm"
-      };
-      ArtifactProcessRequest apr_n = new ArtifactProcessRequest();
-      apr_n.Artifact = new Artifact() {
-        Id = "nuget",
-        Type = "nuget"
-      };
-      await endpoint.Send(apr);
-      await nuget_endpoint.Send(apr_n);
-      await Task.Delay(1000, stoppingToken);
+      await Task.Delay(3000, stoppingToken);
     }
   }
 }
