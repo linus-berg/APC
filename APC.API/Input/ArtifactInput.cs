abaaeb514f149518d61f55a63a61e87c64c8936e
linusberg <linus@fenix.me.uk>
2022-11-01 21:09:30 +0100

    Updated to allow nul filters.

--- APC.API/Input/ArtifactInput.cs
+++ APC.API/Input/ArtifactInput.cs
@@ -3,5 +3,5 @@ namespace APC.API.Input;
 public class ArtifactInput {
   public string Name { get; set; }
   public string Module { get; set; }
-  public string Filter { get; set; }
+  public string? Filter { get; set; }
 }
