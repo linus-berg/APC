bdd7a4ebbb21bd80bfae2d15c7c0365748a06e2c
Linus Berg <linus@fenix.me.uk>
2022-11-14 16:39:33 +0100

    Database innerj oin.

--- APC.Infrastructure/ApcDatabase.cs
+++ APC.Infrastructure/ApcDatabase.cs
@@ -3,37 +3,29 @@ using APC.Services;
 using APC.Services.Models;
 using Dapper;
 using Dapper.Contrib.Extensions;
-using Microsoft.IdentityModel.Protocols;
 using Npgsql;
 
 namespace APC.Infrastructure;
 
 public class ApcDatabase : IApcDatabase {
-  private readonly NpgsqlConnection db_;
   private static readonly string DB_STR_;
+  private readonly NpgsqlConnection db_;
   private NpgsqlTransaction transaction_;
 
   static ApcDatabase() {
     string c_str = Environment.GetEnvironmentVariable("APC_PGSQL_STR");
     DB_STR_ = c_str ?? throw new NoNullAllowedException("Database connection string is null, set APC_PGSQL_STR.");
   }
+
   public ApcDatabase() {
     db_ = new NpgsqlConnection(DB_STR_);
     Open();
     transaction_ = db_.BeginTransaction();
   }
-  
+
   public void Dispose() {
     Dispose(true);
     GC.SuppressFinalize(this);
-  }
-
-  private void Open() {
-    db_.Open();
-  }
-
-  ~ApcDatabase() {
-    Dispose(false);
   }
 
   public async Task<bool> DeleteArtifact(Artifact artifact) {
@@ -58,36 +50,12 @@ public class ApcDatabase : IApcDatabase {
     return id;
   }
 
-  private async Task<ArtifactDependency> AddArtifactDependency(Artifact artifact, ArtifactDependency dependency) {
-    dependency.artifact_id = artifact.id;
-    dependency.id = await db_.InsertAsync(dependency, transaction_);
-    return dependency;
-  }
-
   public async Task<bool> UpdateArtifact(Artifact artifact) {
     return await db_.UpdateAsync(artifact);
   }
 
   public async Task<bool> UpdateArtifact(Artifact current, Artifact updated) {
     return await UpdateArtifactVersions(current, updated);
-  }
-
-  private async Task<bool> UpdateArtifactVersions(Artifact current, Artifact updated) {
-    Dictionary<string, ArtifactVersion> current_versions = await GetVersions(current.id);
-    Dictionary<string, ArtifactVersion> updated_versions = updated.versions;
-    bool has_updated = false;
-    foreach (KeyValuePair<string, ArtifactVersion> kv in updated_versions)
-      if (!current_versions.ContainsKey(kv.Key)) {
-        await AddArtifactVersion(current, kv.Value);
-        has_updated = true;
-      }
-
-    return has_updated;
-  }
-
-  private async Task AddArtifactVersion(Artifact artifact, ArtifactVersion version) {
-    version.artifact_id = artifact.id;
-    version.id = await db_.InsertAsync(version, transaction_);
   }
 
   public async Task<IEnumerable<string>> GetModules() {
@@ -110,6 +78,71 @@ public class ApcDatabase : IApcDatabase {
     );
   }
 
+  public async Task<IEnumerable<Artifact>> GetArtifacts(string module) {
+    Dictionary<int, Artifact> artifacts = new();
+    return (await db_.QueryAsync<Artifact, ArtifactVersion, Artifact>(@"
+        SELECT 
+            * 
+        FROM 
+            artifacts 
+        INNER JOIN
+                artifact_versions ON artifact_id = artifacts.id
+        WHERE 
+            module = @module
+        ", (artifact, version) => {
+        if (!artifacts.TryGetValue(artifact.id, out Artifact entry)) {
+          entry = artifact;
+          entry.versions = new Dictionary<string, ArtifactVersion>();
+          artifacts.Add(artifact.id, entry);
+        }
+
+        if (version != null) entry.versions.Add(version.version, version);
+
+        return entry;
+      },
+      new { module },
+      transaction_
+    )).Distinct();
+  }
+
+  public async Task<IEnumerable<Artifact>> GetRoots(string module) {
+    return await db_.QueryAsync<Artifact>("SELECT * FROM artifacts WHERE module = @module and root = true",
+      new { module },
+      transaction_);
+  }
+
+  private void Open() {
+    db_.Open();
+  }
+
+  ~ApcDatabase() {
+    Dispose(false);
+  }
+
+  private async Task<ArtifactDependency> AddArtifactDependency(Artifact artifact, ArtifactDependency dependency) {
+    dependency.artifact_id = artifact.id;
+    dependency.id = await db_.InsertAsync(dependency, transaction_);
+    return dependency;
+  }
+
+  private async Task<bool> UpdateArtifactVersions(Artifact current, Artifact updated) {
+    Dictionary<string, ArtifactVersion> current_versions = await GetVersions(current.id);
+    Dictionary<string, ArtifactVersion> updated_versions = updated.versions;
+    bool has_updated = false;
+    foreach (KeyValuePair<string, ArtifactVersion> kv in updated_versions)
+      if (!current_versions.ContainsKey(kv.Key)) {
+        await AddArtifactVersion(current, kv.Value);
+        has_updated = true;
+      }
+
+    return has_updated;
+  }
+
+  private async Task AddArtifactVersion(Artifact artifact, ArtifactVersion version) {
+    version.artifact_id = artifact.id;
+    version.id = await db_.InsertAsync(version, transaction_);
+  }
+
   private async Task<Dictionary<string, ArtifactVersion>> GetVersions(int artifact_id) {
     return (await db_.QueryAsync<ArtifactVersion>(
       @"
@@ -125,16 +158,6 @@ public class ApcDatabase : IApcDatabase {
       },
       transaction_
     )).ToDictionary(av => av.version);
-  }
-
-  public async Task<IEnumerable<Artifact>> GetArtifacts(string module) {
-    return await db_.QueryAsync<Artifact>("SELECT * FROM artifacts WHERE module = @module", new { module },
-      transaction_);
-  }
-  
-  public async Task<IEnumerable<Artifact>> GetRoots(string module) {
-    return await db_.QueryAsync<Artifact>("SELECT * FROM artifacts WHERE module = @module and root = true", new { module },
-      transaction_);
   }
 
   private void ReleaseUnmanagedResources() {
