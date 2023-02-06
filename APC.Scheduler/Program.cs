26f5a991951f5e43c565e1cd900d5ad848e24bba
Linus Berg <linus@fenix.me.uk>
2023-02-06 11:03:27 +0100

    Added rudimentary scheduler.

---
+++ APC.Scheduler/Program.cs
@@ -0,0 +1,43 @@
+using APC.Infrastructure;
+using APC.Infrastructure.Services;
+using APC.Kernel;
+using APC.Scheduler;
+using APC.Services;
+using MassTransit;
+using Quartz;
+using StackExchange.Redis;
+
+IHost host = Host.CreateDefaultBuilder(args)
+                 .ConfigureServices(services => {
+                   services.AddHostedService<Worker>();
+                   services.AddMassTransit(mt => {
+                     mt.UsingRabbitMq((ctx, cfg) => {
+                       cfg.SetupRabbitMq();
+                       cfg.ConfigureEndpoints(ctx);
+                     });
+                   });
+                   
+                   services.AddSingleton<IConnectionMultiplexer>(
+                     ConnectionMultiplexer.Connect(
+                       Configuration.GetApcVar(ApcVariable.APC_REDIS_HOST)));
+                   services.AddScoped<IApcDatabase, MongoDatabase>();
+                   services.AddSingleton<IApcCache, ApcCache>();
+                   services.AddScoped<IArtifactService, ArtifactService>();
+
+                   services.AddQuartz(q => {
+                     q.UseMicrosoftDependencyInjectionJobFactory();
+                     q.AddJob<TrackingJob>(
+                       j => j.WithIdentity(TrackingJob.Key));
+                     q.AddTrigger(t => {
+                       t.WithIdentity("tracking-trigger", "apc");
+                       t.ForJob(TrackingJob.Key);
+                       t.WithCronSchedule("* * 0/2 ? * * *");
+                     });
+                   });
+
+                   services.AddQuartzHostedService(q => {
+                     q.WaitForJobsToComplete = true;
+                   });
+                 })
+                 .Build();
+await host.RunAsync();
---
+++ APC.Scheduler/TrackingJob.cs
@@ -0,0 +1,19 @@
+using APC.Services;
+using Quartz;
+
+namespace APC.Scheduler; 
+
+public class TrackingJob : IJob {
+  public static readonly JobKey Key = new JobKey("track-all", "apc");
+  private readonly IArtifactService aps_;
+  private readonly ILogger<TrackingJob> logger_;
+
+  public TrackingJob(ILogger<TrackingJob> logger, IArtifactService aps) {
+    aps_ = aps;
+    logger_ = logger;
+  }
+  public async Task Execute(IJobExecutionContext context) {
+    logger_.LogInformation("Tracking artifacts.");
+    await aps_.ReTrack();
+  }
+}
