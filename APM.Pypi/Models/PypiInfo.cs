b9deac9dc1d0292dbdc09375547cb6dcd2f70747
linusberg <linus@fenix.me.uk>
2023-05-22 21:50:15 +0200

    Fixed pypi

--- APM.Pypi/Models/PypiInfo.cs
+++ APM.Pypi/Models/PypiInfo.cs
@@ -1,11 +1,21 @@
 namespace APM.Pypi.Models;
 
 public class PypiInfo {
-  public List<string> requires_dist { get; set; }
+  public List<string>? requires_dist { get; set; }
 
   public List<string> GetDependencies() {
     List<string> dependencies = new();
+    if (requires_dist == null) {
+      return dependencies;
+    }
+
     foreach (string dist in requires_dist) {
+      string dependency = dist.Split(' ')[0];
+      if (dependency.Contains("=") || dependency.Contains("<") ||
+          dependency.Contains(">") || dependency.Contains(";")) {
+        continue;
+      }
+
       dependencies.Add(dist.Split(' ')[0]);
     }
 
