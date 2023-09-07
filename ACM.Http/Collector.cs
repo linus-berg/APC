889707dbfa4406c6ee72c7f1b2d234c6f36a66a8
linus-berg <linus@fenix.me.uk>
2023-09-07 18:58:03 +0200

    Added force collection.

--- ACM.Http/Collector.cs
+++ ACM.Http/Collector.cs
@@ -16,7 +16,7 @@ public class Collector : ICollector {
     string location = context.Message.location;
     string module = context.Message.module;
     string fp = fs_.GetArtifactPath(module, location);
-    if (!fs_.Exists(fp) && !fs_.Exists(fp + ".tmp")) {
+    if ((!fs_.Exists(fp) && !fs_.Exists(fp + ".tmp")) || context.Message.force) {
       RemoteFile rf = new(location);
       if (await rf.Get(fp)) {
         fs_.CreateDailyLink(module, location);
--- APC.Kernel/Messages/ArtifactCollectRequest.cs
+++ APC.Kernel/Messages/ArtifactCollectRequest.cs
@@ -4,6 +4,8 @@ public class ArtifactCollectRequest {
   public string location { get; set; }
   public string module { get; set; }
 
+  public bool force { get; set; } = false;
+
   public string GetCollectorModule() {
     Uri uri = new(location);
     string scheme = uri.Scheme;
