efe6d5b64ee77f3088a2f2afaa9fe4a95bd603c6
wufh43 <user@localhost.localdomain>
2024-02-20 11:23:34 +0100

    Added ability to filter artifact versions using semver ranges.
    The new field `filterType` on `Artifact` determines how the filter
    should be interpreted.

--- ACM.Router/Router.cs
+++ ACM.Router/Router.cs
@@ -3,19 +3,19 @@ using APC.Kernel.Extensions;
 using APC.Kernel.Messages;
 using APC.Kernel.Models;
 using MassTransit;
+using Semver;
 
 namespace ACM.Router;
 
 public class Router : IConsumer<ArtifactRouteRequest> {
   public async Task Consume(ConsumeContext<ArtifactRouteRequest> context) {
     Artifact artifact = context.Message.artifact;
-    Regex? regex = null;
-    if (!string.IsNullOrEmpty(artifact.filter)) {
-      regex = new Regex(artifact.filter);
-    }
+
+    Predicate<string> artifactFilter = CreateFilterFunction(artifact);
 
     foreach (KeyValuePair<string, ArtifactVersion> kv in artifact.versions) {
-      bool collect = regex == null || regex.IsMatch(kv.Key);
+      bool collect = artifactFilter(kv.Key);
+
       if (!collect) {
         continue;
       }
@@ -26,6 +26,29 @@ public class Router : IConsumer<ArtifactRouteRequest> {
                                 ? artifact.processor
                                 : file.Value.folder);
       }
+    }
+  }
+
+  private static Predicate<string> NO_FILTER = s => true;
+
+  private static Predicate<string> CreateFilterFunction(Artifact artifact) {
+    if (string.IsNullOrEmpty(artifact.filter)) {
+        return NO_FILTER;
+    }
+
+    switch (artifact.filterType) {
+      case ArtifactFilterType.SemverRange:
+        SemVersionRange versionRange = SemVersionRange.Parse(artifact.filter);
+        return (s) => {
+          SemVersion version = SemVersion.Parse(s, SemVersionStyles.Any);
+          return versionRange.Contains(version);
+        };
+      case ArtifactFilterType.Regex:
+        goto default;
+      default:
+        // fallback to regex, for backwards compatibility
+        Regex regex = new Regex(artifact.filter);
+        return (s) => regex.IsMatch(s);
     }
   }
 }
