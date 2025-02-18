942d4626a394a1b1fb951dd72bb9e4f4f1bf5f0b
wufh43 <user@localhost.localdomain>
2025-02-18 09:26:16 +0100

    npm: Support all dependency structures in `Package` metadata.
    Fixes "Metadata error ..." when deserializing `Metadata` response.
    
    Previously, dependencies were typed as `Dictionary<string,string>`,
    but some metadata documents contain dependencies that are actually
    `Dictionary<string,object>`, for example `deep-diff`:
    
    ```
    ...
         "devDependencies": {
            "lodash": {
              "version": "0.9.2"
            },
            "extend": {
              "version": "1.1.1"
            },
            ...
         }
    ...
    ```
    
    Fix by changing to `Dictionary<string, JsonElemement>`. Currently, the
    code does not actually use the dictionary value.

--- APM.Npm/Models/Package.cs
+++ APM.Npm/Models/Package.cs
@@ -1,8 +1,10 @@
+using System.Text.Json;
+
 namespace APM.Npm.Models;
 
 public class Package {
-  public Dictionary<string, string>? dependencies { get; set; }
-  public Dictionary<string, string>? peerDependencies { get; set; }
-  public Dictionary<string, string>? devDependencies { get; set; }
+  public Dictionary<string, JsonElement>? dependencies { get; set; }
+  public Dictionary<string, JsonElement>? peerDependencies { get; set; }
+  public Dictionary<string, JsonElement>? devDependencies { get; set; }
   public Distribution dist { get; set; }
 }
